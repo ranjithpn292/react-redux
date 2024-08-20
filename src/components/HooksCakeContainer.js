8cd88ff9a9ac130d95a3bca1a036d7945c96f79d
ranjithpn292 <ranjithpn292@gmail.com>
2024-08-20 22:47:16 +0530

    added redux files using useSelector,useDispatch hooks

---
+++ src/components/HooksCakeContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {useSelector, useDispatch} from 'react-redux';
+import { buyCake } from '../redux/cakes/cakeActions';
+
+function HooksCakeContainer() {
+   const numOfCakes =  useSelector(state => state.numOfCakes)
+   const dispatch = useDispatch()
+  return (
+    <div>
+    <div>Num of cakes - {numOfCakes}</div>
+    <button onClick={()=>dispatch(buyCake())}>buy cakes</button>
+    </div>
+  )
+}
+
+export default HooksCakeContainer
